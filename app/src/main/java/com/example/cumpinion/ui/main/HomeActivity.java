a2d03acd101fa0374bac5d3aad55f5db15621380
Kk0lt <lyesaid29@gmail.com>
2024-02-07 08:19:03 -0500

    Added bottom navigation bar and communication between fragments

---
+++ app/src/main/java/com/example/cumpinion/ui/main/HomeActivity.java
@@ -0,0 +1,49 @@
+package com.example.cumpinion.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import com.example.cumpinion.HistoryFragment;
+import com.example.cumpinion.HomeFragment;
+import com.example.cumpinion.LeaderboardFragment;
+import com.example.cumpinion.R;
+import com.example.cumpinion.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+     ActivityHomeBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            }
+            else if (item.getItemId() == R.id.leaderboard) {
+                replaceFragment(new LeaderboardFragment());
+            }
+            else if (item.getItemId() == R.id.history) {
+                replaceFragment(new HistoryFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
