832d9866423d40cd977ac879e310ca85b290f675
Kk0lt <lyesaid29@gmail.com>
2024-02-09 10:38:43 -0500

    Main Activity replaced with HomeActivity

--- app/src/main/java/com/example/cumpinion/LeaderboardFragment.java
+++ app/src/main/java/com/example/cumpinion/LeaderboardFragment.java
@@ -11,8 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.cumpinion.ui.main.HomeActivity;
-
 import java.util.ArrayList;
 import java.util.List;
 
